39e5d6a39d4774eb857d75d73c8936551000dcd6
hugovital <hugovital@gmail.com>
2019-05-28 15:03:06 -0300

    codigo com jedis

---
+++ src/main/java/testes/redis/Main.java
@@ -0,0 +1,44 @@
+package testes.redis;
+
+import redis.clients.jedis.Jedis;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		
+		String remote_ip = "3.18.110.177";
+		
+		// Connecting to Redis on localhost		
+		//jedis.auth("forefather");
+		
+		// adding a new key
+		Jedis jedis = new Jedis( remote_ip );
+		jedis.set("key", "COWABANGA IT IS!!!!");
+		jedis.disconnect();
+
+		while ( true ) {
+			
+			try {
+			
+				jedis = new Jedis( remote_ip );
+				System.out.println(jedis.get("key"));
+							
+			} catch (Exception ex) {
+				
+				System.out.println( ex.getMessage() );
+				
+			} finally {
+				
+				jedis.disconnect();				
+				
+				Thread.sleep(1000);
+				
+			}
+				
+			
+		}
+
+		//System.out.println("Done.");
+	}
+
+}
